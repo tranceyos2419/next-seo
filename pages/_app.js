c81e6c69de04955e0e2c38f4538d274d1eb6f454
tranceyos2419 <tranceyos2419@gmail.com>
2019-04-17 22:09:00 +0900

    add nex-ga

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import App, { Container } from "next/app";
+import Head from "next/head";
+import Router from "next/router";
+import withGA from "next-ga";
+
+class MyApp extends App {
+  render() {
+    const { pageProps } = this.props;
+    return (
+      <Container>
+        <Head />
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
+
+export default withGA("UA-138515076-1", Router)(MyApp);
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138515076-1"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', '');
+// </script>
