bbff120697553447db1bce35f825044ce2c00d8d
tranceyos2419 <tranceyos2419@gmail.com>
2019-04-17 22:33:08 +0900

    test

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header>
-      <h1>Next.js Example on Now 2.0 + Google Analytics</h1>
+      <h1>Next.js Example on Now 3.0 + Google Analytics</h1>
     </header>
   );
 }
